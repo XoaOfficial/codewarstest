c8241d52b33931e28934ba61788530f1201b6f59
XoaOfficial <88820651+XoaOfficial@users.noreply.github.com>
2022-03-07 16:45:36 +0100

    fibionaqui
    fibionaqui

--- fibionaqui.py
+++ fibionaqui.py
@@ -1,5 +1,9 @@
-pip install sentiment-analysis-spanish
-pip install keras tensorflow
-from sentiment_analysis_spanish import sentiment_analysis
-sentiment = sentiment_analysis.SentimentAnalysisSpanish()
-print(sentiment.sentiment("menuda mierda que es el codewars"))
+fibionaqui = []
+fibionaqui.append(int(input()))
+fibionaqui.append(int(input()))
+num = int(input())
+for i in range (num-2):
+    fibionaqui.append(fibionaqui[i]+fibionaqui[i+1])
+for i in range(len(fibionaqui)):
+    fibionaqui[i] = str(fibionaqui[i])
+print(",".join([i for i in fibionaqui]))
